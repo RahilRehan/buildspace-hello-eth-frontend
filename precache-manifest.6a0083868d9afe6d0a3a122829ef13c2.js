a24b4bcf7a5368f5e8a9d017355125a874adce0a
RahilRehan <rahil.rehan@thoughtworks.com>
2022-01-07 10:56:08 +0530

    Updates

---
+++ precache-manifest.6a0083868d9afe6d0a3a122829ef13c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e2dec854f1e7c45a45e06f205d14f3",
+    "url": "/buildspace-hello-eth-frontend/index.html"
+  },
+  {
+    "revision": "7dbcfc737172ffd7e325",
+    "url": "/buildspace-hello-eth-frontend/static/css/main.0e632e19.chunk.css"
+  },
+  {
+    "revision": "6c9176181fb02fa12279",
+    "url": "/buildspace-hello-eth-frontend/static/js/2.e347e28f.chunk.js"
+  },
+  {
+    "revision": "7dbcfc737172ffd7e325",
+    "url": "/buildspace-hello-eth-frontend/static/js/main.9469ab53.chunk.js"
+  },
+  {
+    "revision": "b920768b61773f7ce9fd",
+    "url": "/buildspace-hello-eth-frontend/static/js/runtime-main.e2323242.js"
+  }
+]);
